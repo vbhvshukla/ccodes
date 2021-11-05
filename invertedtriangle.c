bd8624e146c6d71b34f3c6db63d422bfd519b27b
Vaibhav Shukla <51282485+vbhvshukla@users.noreply.github.com>
2021-11-05 17:54:48 +0530

    Create invertedtriangle.c

---
+++ invertedtriangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0;i<4;i++){
+        for (int j=0 ;j<i;j++){
+            printf(" ");
+        }
+        for (int j=i ;j<4;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
